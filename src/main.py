a974f0aef19151ca09a20dbd5f331fc0ef6b27b4
Neucrack <CZD666666@gmail.com>
2017-07-17 01:56:58 +0800

    some UI complete

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import sys
 from src import parameters
 from PyQt5.QtWidgets import (QApplication, QWidget,QToolTip,QPushButton,QMessageBox,QDesktopWidget,QMainWindow,
-                             QVBoxLayout,QHBoxLayout,QGridLayout,QTextEdit,QComboBox)
+                             QVBoxLayout,QHBoxLayout,QGridLayout,QTextEdit,QComboBox,QLabel,QRadioButton,QCheckBox,
+                             QLineEdit,QGroupBox)
 from PyQt5.QtGui import QIcon,QFont
 
 
@@ -26,8 +27,12 @@ class MainWindow(QMainWindow):
         mainLayout.addLayout(sendReceiveLayout)
         mainLayout.addLayout(sendFunctionalLayout)
         mainWidget.setLayout(mainLayout)
+        mainLayout.setStretch(0,2)
+        mainLayout.setStretch(1, 6)
+        mainLayout.setStretch(2, 2)
         self.setCentralWidget(mainWidget)
-        # widgets
+
+        # widgets receive and send area
         receiveArea = QTextEdit()
         sendArea = QTextEdit()
         sendButtion = QPushButton(parameters.strSend)
@@ -35,7 +40,6 @@ class MainWindow(QMainWindow):
         sendAreaWidgetsLayout = QHBoxLayout()
         sendAreaWidgetsLayout.addWidget(sendArea)
         sendAreaWidgetsLayout.addWidget(sendButtion)
-
         sendReceiveLayout.addWidget(receiveArea)
         sendReceiveLayout.addLayout(sendAreaWidgetsLayout)
         sendReceiveLayout.addWidget(sendHistory)
@@ -43,6 +47,72 @@ class MainWindow(QMainWindow):
         sendReceiveLayout.setStretch(1, 2)
         sendReceiveLayout.setStretch(2, 1)
 
+        # widgets serial settings
+        serialSettingsGroupBox = QGroupBox(parameters.strSerialSettings)
+        serialSettingsLayout = QGridLayout()
+        serialReceiveSettingsLayout = QGridLayout()
+        serialSendSettingsLayout = QGridLayout()
+        serialPortLabek = QLabel(parameters.strSerialPort)
+        serailBaudrateLabel = QLabel(parameters.strSerialBaudrate)
+        serailBytesLabel = QLabel(parameters.strSerialBytes)
+        serailParityLabel = QLabel(parameters.strSerialParity)
+        serailStopbitsLabel = QLabel(parameters.strSerialStopbits)
+        serialPortCombobox = QComboBox()
+        serailBaudrateCombobox = QComboBox()
+        serailBytesCombobox = QComboBox()
+        serailParityCombobox = QComboBox()
+        serailStopbitsCombobox = QComboBox()
+        serialOpenCloseButton = QPushButton(parameters.strOpen)
+        serialSettingsLayout.addWidget(serialPortLabek,0,0)
+        serialSettingsLayout.addWidget(serailBaudrateLabel, 1, 0)
+        serialSettingsLayout.addWidget(serailBytesLabel, 2, 0)
+        serialSettingsLayout.addWidget(serailParityLabel, 3, 0)
+        serialSettingsLayout.addWidget(serailStopbitsLabel, 4, 0)
+        serialSettingsLayout.addWidget(serialPortCombobox, 0, 1)
+        serialSettingsLayout.addWidget(serailBaudrateCombobox, 1, 1)
+        serialSettingsLayout.addWidget(serailBytesCombobox, 2, 1)
+        serialSettingsLayout.addWidget(serailParityCombobox, 3, 1)
+        serialSettingsLayout.addWidget(serailStopbitsCombobox, 4, 1)
+        serialSettingsLayout.addWidget(serialOpenCloseButton, 5, 0,1,2)
+        serialSettingsGroupBox.setLayout(serialSettingsLayout)
+        settingLayout.addWidget(serialSettingsGroupBox)
+
+        # serial receive settings
+        serialReceiveSettingsGroupBox = QGroupBox(parameters.strSerialReceiveSettings)
+        receiveSettingsAscii = QRadioButton(parameters.strAscii)
+        receiveSettingsHex = QRadioButton(parameters.strHex)
+        receiveSettingsAutoLinefeed = QCheckBox(parameters.strAutoLinefeed)
+        receiveSettingsAutoLinefeedTime = QLineEdit(parameters.strAutoLinefeedTime)
+        receiveSettingsAutoLinefeed.setMaximumWidth(75)
+        receiveSettingsAutoLinefeedTime.setMaximumWidth(75)
+        serialReceiveSettingsLayout.addWidget(receiveSettingsAscii,1,0,1,1)
+        serialReceiveSettingsLayout.addWidget(receiveSettingsHex,1,1,1,1)
+        serialReceiveSettingsLayout.addWidget(receiveSettingsAutoLinefeed, 2, 0, 1, 1)
+        serialReceiveSettingsLayout.addWidget(receiveSettingsAutoLinefeedTime, 2, 1, 1, 1)
+        serialReceiveSettingsGroupBox.setLayout(serialReceiveSettingsLayout)
+        settingLayout.addWidget(serialReceiveSettingsGroupBox)
+
+        # serial send settings
+        serialSendSettingsGroupBox = QGroupBox(parameters.strSerialSendSettings)
+        sendSettingsAscii = QRadioButton(parameters.strAscii)
+        sendSettingsHex = QRadioButton(parameters.strHex)
+        sendSettingsScheduledLabel = QCheckBox(parameters.strScheduled)
+        sendSettingsScheduled = QLineEdit(parameters.strScheduledTime)
+        sendSettingsScheduledLabel.setMaximumWidth(75)
+        sendSettingsScheduled.setMaximumWidth(75)
+        serialSendSettingsLayout.addWidget(sendSettingsAscii,1,0,1,1)
+        serialSendSettingsLayout.addWidget(sendSettingsHex,1,1,1,1)
+        serialSendSettingsLayout.addWidget(sendSettingsScheduledLabel, 2, 0, 1, 1)
+        serialSendSettingsLayout.addWidget(sendSettingsScheduled, 2, 1, 1, 1)
+        serialSendSettingsGroupBox.setLayout(serialSendSettingsLayout)
+        settingLayout.addWidget(serialSendSettingsGroupBox)
+
+        settingLayout.setStretch(0, 5)
+        settingLayout.setStretch(1, 2.5)
+        settingLayout.setStretch(2, 2.5)
+
+
+        # main window
         self.statusBar().showMessage('Ready')
         self.resize(800, 500)
         self.MoveToCenter()
