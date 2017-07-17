d837555d6171f6b0e83412818040a8f3e1cc2b8b
Neucrack <CZD666666@gmail.com>
2017-07-17 13:56:30 +0800

    basic UI complete

--- src/main.py
+++ src/main.py
@@ -4,7 +4,10 @@ from PyQt5.QtWidgets import (QApplication, QWidget,QToolTip,QPushButton,QMessage
                              QVBoxLayout,QHBoxLayout,QGridLayout,QTextEdit,QComboBox,QLabel,QRadioButton,QCheckBox,
                              QLineEdit,QGroupBox)
 from PyQt5.QtGui import QIcon,QFont
-
+import serial
+import serial.tools.list_ports
+import threading
+import time
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -35,11 +38,16 @@ class MainWindow(QMainWindow):
         # widgets receive and send area
         receiveArea = QTextEdit()
         sendArea = QTextEdit()
+        clearReceiveButtion = QPushButton(parameters.strClearReceive)
         sendButtion = QPushButton(parameters.strSend)
         sendHistory = QComboBox()
         sendAreaWidgetsLayout = QHBoxLayout()
+        buttonLayout = QVBoxLayout()
+        buttonLayout.addWidget(clearReceiveButtion)
+        buttonLayout.addStretch(1)
+        buttonLayout.addWidget(sendButtion)
         sendAreaWidgetsLayout.addWidget(sendArea)
-        sendAreaWidgetsLayout.addWidget(sendButtion)
+        sendAreaWidgetsLayout.addLayout(buttonLayout)
         sendReceiveLayout.addWidget(receiveArea)
         sendReceiveLayout.addLayout(sendAreaWidgetsLayout)
         sendReceiveLayout.addWidget(sendHistory)
@@ -57,23 +65,39 @@ class MainWindow(QMainWindow):
         serailBytesLabel = QLabel(parameters.strSerialBytes)
         serailParityLabel = QLabel(parameters.strSerialParity)
         serailStopbitsLabel = QLabel(parameters.strSerialStopbits)
-        serialPortCombobox = QComboBox()
-        serailBaudrateCombobox = QComboBox()
-        serailBytesCombobox = QComboBox()
-        serailParityCombobox = QComboBox()
-        serailStopbitsCombobox = QComboBox()
-        serialOpenCloseButton = QPushButton(parameters.strOpen)
+        self.serialPortCombobox = QComboBox()
+        self.serailBaudrateEditText = QLineEdit()
+        self.serailBaudrateEditText.setText(parameters.strBaudRateDefault)
+        self.serailBytesCombobox = QComboBox()
+        self.serailBytesCombobox.addItem("5")
+        self.serailBytesCombobox.addItem("6")
+        self.serailBytesCombobox.addItem("7")
+        self.serailBytesCombobox.addItem("8")
+        self.serailBytesCombobox.setCurrentIndex(3)
+        self.serailParityCombobox = QComboBox()
+        self.serailParityCombobox.addItem("None")
+        self.serailParityCombobox.addItem("Odd")
+        self.serailParityCombobox.addItem("Even")
+        self.serailParityCombobox.addItem("Mark")
+        self.serailParityCombobox.addItem("Space")
+        self.serailParityCombobox.setCurrentIndex(0)
+        self.serailStopbitsCombobox = QComboBox()
+        self.serailStopbitsCombobox.addItem("1")
+        self.serailStopbitsCombobox.addItem("1.5")
+        self.serailStopbitsCombobox.addItem("2")
+        self.serailStopbitsCombobox.setCurrentIndex(0)
+        self.serialOpenCloseButton = QPushButton(parameters.strOpen)
         serialSettingsLayout.addWidget(serialPortLabek,0,0)
         serialSettingsLayout.addWidget(serailBaudrateLabel, 1, 0)
         serialSettingsLayout.addWidget(serailBytesLabel, 2, 0)
         serialSettingsLayout.addWidget(serailParityLabel, 3, 0)
         serialSettingsLayout.addWidget(serailStopbitsLabel, 4, 0)
-        serialSettingsLayout.addWidget(serialPortCombobox, 0, 1)
-        serialSettingsLayout.addWidget(serailBaudrateCombobox, 1, 1)
-        serialSettingsLayout.addWidget(serailBytesCombobox, 2, 1)
-        serialSettingsLayout.addWidget(serailParityCombobox, 3, 1)
-        serialSettingsLayout.addWidget(serailStopbitsCombobox, 4, 1)
-        serialSettingsLayout.addWidget(serialOpenCloseButton, 5, 0,1,2)
+        serialSettingsLayout.addWidget(self.serialPortCombobox, 0, 1)
+        serialSettingsLayout.addWidget(self.serailBaudrateEditText, 1, 1)
+        serialSettingsLayout.addWidget(self.serailBytesCombobox, 2, 1)
+        serialSettingsLayout.addWidget(self.serailParityCombobox, 3, 1)
+        serialSettingsLayout.addWidget(self.serailStopbitsCombobox, 4, 1)
+        serialSettingsLayout.addWidget(self.serialOpenCloseButton, 5, 0,1,2)
         serialSettingsGroupBox.setLayout(serialSettingsLayout)
         settingLayout.addWidget(serialSettingsGroupBox)
 
@@ -111,6 +135,13 @@ class MainWindow(QMainWindow):
         settingLayout.setStretch(1, 2.5)
         settingLayout.setStretch(2, 2.5)
 
+        # right functional layout
+        addButton = QPushButton(parameters.strAdd)
+        functionalGroupBox = QGroupBox(parameters.strFunctionalSend)
+        functionalGridLayout = QGridLayout()
+        functionalGridLayout.addWidget(addButton,0,1)
+        functionalGroupBox.setLayout(functionalGridLayout)
+        sendFunctionalLayout.addWidget(functionalGroupBox)
 
         # main window
         self.statusBar().showMessage('Ready')
@@ -138,9 +169,36 @@ class MainWindow(QMainWindow):
         else:
             event.ignore()
 
+    def findSerialPort(self):
+        port_list = list(serial.tools.list_ports.comports())
+        return port_list
+
+    def portChanged(self):
+        self.serialPortCombobox.setCurrentIndex(0)
+        self.serialPortCombobox.setToolTip(str(portList[0]))
+
+    def detectSerialPort(self):
+        self.serialPortCombobox.clear()
+        while(1):
+            portList = self.findSerialPort();
+            for i in portList:
+                self.serialPortCombobox.addItem(str(i[0])+" "+str(i[1]))
+            if len(portList)>0:
+                self.serialPortCombobox.setCurrentIndex(0)
+                self.serialPortCombobox.setToolTip(str(portList[0]))
+                break
+        return
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     mainWindow = MainWindow()
+    threads = []
+    t1 = threading.Thread(target=mainWindow.detectSerialPort)
+    threads.append(t1)
+    for t in threads:
+        t.setDaemon(True)
+        t.start()
     sys.exit(app.exec_())
 
 
