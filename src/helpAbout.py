c06ac09cb818405aa7062dc1501731dc6bef5584
Neucrack <CZD666666@gmail.com>
2017-07-21 09:00:42 +0800

    update help

--- src/helpAbout.py
+++ src/helpAbout.py
@@ -2,10 +2,13 @@ versionMajor = 1
 versionMinor = 0
 date = "2017.7.20"
 strAbout = '''\
+Python 3.6.1 + PyQt5<br><br>
 <div><div>PySerialAssistant is a Open source project create by </div><a style="vertical-align: middle;" href="http://www.neucrack.com"><img src='../assets/logo2.png' width=109 height=32></img></a></div><br><br>
 
 See more on <b><a href="https://github.com/neutree/PySerialAssistant.git">Github</a></b><br><br>
 Welcome to improve it together<br><br>
+
+
 Shortcut:<br>
 <b style="color:red;"><kbd>Ctrl+Enter</kbd></b>: Send data<br> 
 
