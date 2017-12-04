8d3814509a1091fffb0d9b1a6a5b8d35069aed48
Neucrack <CZD666666@gmail.com>
2017-12-04 22:49:14 +0800

    fix file name error

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from COMTool import helpAbout,parameters
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.MD'), encoding='utf-8') as f:
     long_description = f.read()
 
 
