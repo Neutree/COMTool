2e280fe870a7e557200f9d26ab5d80294bbecce7
Neucrack <CZD666666@gmail.com>
2020-07-21 16:21:36 +0800

    optimize combobox

--- COMTool/Combobox.py
+++ COMTool/Combobox.py
@@ -4,17 +4,24 @@ from PyQt5.QtCore import pyqtSignal
 
 class ComboBox(QComboBox):
     clicked = pyqtSignal()
+    # popupAboutToBeShown = pyqtSignal()
+
     def __init__(self):
         QComboBox.__init__(self)
         listView = QListView()
         self.setView(listView)
         return
 
-    def __del__(self):
-        return
-
     def mouseReleaseEvent(self, QMouseEvent):
-        self.showPopup()
+        self.showItems()
+
+    def showPopup(self):
+        # self.popupAboutToBeShown.emit()
+        # prevent show popup, manually call it in mouse release event
+        pass
+    
+    def showItems(self):
+        super(ComboBox, self).showPopup()
 
     def mousePressEvent(self, QMouseEvent):
         self.clicked.emit()
