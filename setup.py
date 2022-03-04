bde2eb06b9f232899517945c786e1462c2f136ab
Neucrack <CZD666666@gmail.com>
2022-03-04 14:14:12 +0800

    update pack script and readme

--- setup.py
+++ setup.py
@@ -107,7 +107,7 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-          'COMTool': ['assets/*', "assets/qss/*", "locales/*/*/*.?o"],
+          'COMTool': ['assets/*', "assets/qss/*", "locales/*/*/*.?o", "protocols/*"],
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
