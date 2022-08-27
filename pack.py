976881dbbe92ddea4ad351c16ab9456ca95911e0
Neucrack <CZD666666@gmail.com>
2022-08-27 14:04:23 +0800

    fix ci error

--- pack.py
+++ pack.py
@@ -10,6 +10,14 @@ if sys.version_info < (3, 7):
     print("only support python >= 3.7, but now is {}".format(sys.version_info))
     sys.exit(1)
 
+# when execute packed executable program(./dist/comtool) warning missing package, add here to resolve
+hidden_imports = [
+    "pyqtgraph.graphicsItems.PlotItem.plotConfigTemplate_pyqt5",
+    "pyqtgraph.graphicsItems.ViewBox.axisCtrlTemplate_pyqt5",
+    "pyqtgraph.imageview.ImageViewTemplate_pyqt5",
+    "babel.numbers"
+]
+
 
 linux_out = "comtool_ubuntu_v{}.tar.xz".format(version.__version__)
 macos_out = "comtool_macos_v{}.dmg".format(version.__version__)
@@ -85,11 +93,14 @@ def pack():
     if os.path.exists("COMTool/__pycache__"):
         shutil.rmtree("COMTool/__pycache__")
 
+    hidden_imports_str = ""
+    for item in hidden_imports:
+        hidden_imports_str += f'--hidden-import {item} '
     if sys.platform.startswith("win32"):
-        cmd = 'pyinstaller --hidden-import babel.numbers -p "COMTool" --add-data="COMTool/assets;assets" --add-data="COMTool/locales;locales" --add-data="COMTool/protocols;protocols" --add-data="README.MD;./" --add-data="README_ZH.MD;./" -i="COMTool/assets/logo.ico" -w COMTool/Main.py -n comtool'
+        cmd = f'pyinstaller {hidden_imports_str} -p "COMTool" --add-data="COMTool/assets;assets" --add-data="COMTool/locales;locales" --add-data="COMTool/protocols;protocols" --add-data="README.MD;./" --add-data="README_ZH.MD;./" -i="COMTool/assets/logo.ico" -w COMTool/Main.py -n comtool'
     elif sys.platform.startswith("darwin"):
         # macos not case insensitive, so can not contain comtool file and COMTool dir, so we copy to binary root dir
-        cmd = 'pyi-makespec --hidden-import babel.numbers -p "COMTool" --add-data="COMTool/assets:assets" --add-data="COMTool/locales:locales" --add-data="COMTool/protocols:protocols" --add-data="README_ZH.MD:./" --add-data="README.MD:./" -i="COMTool/assets/logo.icns" -w COMTool/Main.py  -n comtool'
+        cmd = f'pyi-makespec {hidden_imports_str} -p "COMTool" --add-data="COMTool/assets:assets" --add-data="COMTool/locales:locales" --add-data="COMTool/protocols:protocols" --add-data="README_ZH.MD:./" --add-data="README.MD:./" -i="COMTool/assets/logo.icns" -w COMTool/Main.py  -n comtool'
         ret = os.system(cmd)
         if ret != 0:
             raise Exception("pack failed")
@@ -105,7 +116,7 @@ def pack():
         print("-- update bundle for macos build complete")
         cmd = 'pyinstaller comtool.spec'
     else:
-        cmd = 'pyinstaller --hidden-import babel.numbers -p "COMTool" --add-data="COMTool/assets:assets" --add-data="COMTool/locales:locales" --add-data="COMTool/protocols:protocols" --add-data="README.MD:./" --add-data="README_ZH.MD:./" -i="COMTool/assets/logo.ico" -w COMTool/Main.py -n comtool'
+        cmd = f'pyinstaller {hidden_imports_str} -p "COMTool" --add-data="COMTool/assets:assets" --add-data="COMTool/locales:locales" --add-data="COMTool/protocols:protocols" --add-data="README.MD:./" --add-data="README_ZH.MD:./" -i="COMTool/assets/logo.ico" -w COMTool/Main.py -n comtool'
 
     print("-- execute:", cmd)
     ret = os.system(cmd)
